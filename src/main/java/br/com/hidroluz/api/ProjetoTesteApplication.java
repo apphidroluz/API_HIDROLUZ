a700ecbea9705610755c4986291bf6267c2c4114
apphidroluz <apphidroluz@gmail.com>
2018-09-12 10:54:33 -0300

    Versão 1

---
+++ src/main/java/br/com/hidroluz/api/ProjetoTesteApplication.java
@@ -0,0 +1,25 @@
+package br.com.hidroluz.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProjetoTesteApplication {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtd;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTesteApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println("### QTD = " + this.qtd);
+		};
+	}
+}
