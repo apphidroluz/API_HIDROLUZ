50c803fcab4db1c067f2b2b836cfad76a576d9bd
Informatica02 <apphidroluz@gmail.com>
2018-09-14 08:45:47 -0300

    version 4

--- src/main/java/br/com/hidroluz/api/dtos/ClienteDTO.java
+++ src/main/java/br/com/hidroluz/api/dtos/ClienteDTO.java
@@ -1,5 +1,9 @@
 package br.com.hidroluz.api.dtos;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 public class ClienteDTO {
 
 	private Integer id_Cliente;
@@ -23,14 +27,19 @@ public class ClienteDTO {
 		this.id_Cliente = id_Cliente;
 	}
 
+	@NotEmpty(message = "Login não pode ficar vazio")
+	@Length(min = 3, max = 200, message = "Login deve possuir no mínimo 3 caracteres")
 	public String getLogin() {
 		return login;
 	}
 
+	
 	public void setLogin(String login) {
 		this.login = login;
 	}
 
+	@NotEmpty(message = "Senha não pode ficar vazia")
+	@Length(min = 3, max = 200, message = "Senha deve conter no mínimo 4 caracteres")
 	public String getSenha() {
 		return senha;
 	}
